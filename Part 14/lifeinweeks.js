5828c35cf952123cc34afdfec44fb887634d614b
Eba-Teshale-Intellicore <ebateshale40@gmail.com>
2025-09-15 01:13:31 +0300

    Add files via upload

---
+++ Part 14/lifeinweeks.js
@@ -0,0 +1,6 @@
+function Age(Yourage){
+    var x = (90-Yourage)*365;
+    var y = (90-Yourage)*52;
+    var z = (90-Yourage)*12;
+    console.log("You have "+ x + " days, " + y + " weeks, and " + z + " months left. ");
+}
