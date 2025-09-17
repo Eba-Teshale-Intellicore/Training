ed82f2e842a19eca0b1621f39d97077085dca5cd
Eba-Teshale-Intellicore <ebateshale40@gmail.com>
2025-09-17 05:01:49 +0300

    Add files via upload

--- Part 16/DOM_Challege_Starting_files/DOM Challenge Starting Files/index.js
+++ Part 16/DOM_Challege_Starting_files/DOM Challenge Starting Files/index.js
@@ -1,1 +1,5 @@
-document.query
+// Change the <h1>
+document.firstElementChild.lastElementChild.firstElementChild.innerHTML = "Eba";
+
+// Change the third <li> in the <ul>
+document.firstElementChild.lastElementChild.children[3].lastElementChild.innerHTML = "Ebas";
